b6165a7c027b5808a23d1dc9239bc8b51912a11e
Ulises Ramirez-Roche <ulisesrmzroche@gmail.com>
2021-03-18 00:37:31 +0000

    feat: start of new reservation form

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueFormulate from '@braid/vue-formulate'
 
 Vue.config.productionTip = false
+Vue.use(VueFormulate)
 
 new Vue({
   router,
--- frontend/src/views/NewReservation.vue
+++ frontend/src/views/NewReservation.vue
@@ -1,11 +1,53 @@
 <template>
   <div>
     <h1>New Reservation</h1>
+    <FormulateForm
+      v-model="reservation"
+    >
+    <FormulateInput
+      name="name"
+      label="What is your name?"
+      validation="required"
+    />
+    <FormulateInput
+      name="email"
+      label="What is your email?"
+      validation="required|text|max:120"
+    />
+    <FormulateInput
+      name="partySize"
+      label="What is your party size?"
+      validation="required|number|max:10"
+      type="select"
+    />
+    <FormulateInput
+      name="date"
+      label="What is the reservation date??"
+      validation="required|text|max:120"
+      type="select"
+    />
+    <FormulateInput
+      name="time"
+      label="What is the reservation time??"
+      validation="required|text|max:120"
+      type="select"
+    />
+    <FormulateInput
+      type="submit"
+      label="Sign up"
+    />
+    </FormulateForm>
   </div>
+  
 </template>
 <script>
 
 export default {
   name: 'NewReservation',
+    data () {
+    return {
+      reservation: {}
+    }
+  }
 }
 </script>
