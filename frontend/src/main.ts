c6a507e99861e587924b8891c4bb4ef1980b3aa4
Ulises Ramirez-Roche <ulisesrmzroche@gmail.com>
2021-03-18 06:46:55 +0000

    feat: install bulma

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueFormulate from '@braid/vue-formulate'
-import FormulateVueDatetimePlugin from '@cone2875/vue-formulate-datetime'
-import 'vue-datetime/dist/vue-datetime.css'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
 Vue.config.productionTip = false
 
 Vue.use(VueFormulate, {
-  plugins: [ FormulateVueDatetimePlugin ]
+  plugins: [ ]
 })
+
+Vue.use(Buefy)
 
 
 new Vue({
